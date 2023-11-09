089c477f8ae4c89e38342ba8495bca3eaa8b70dc
jon-lee1998 <149186400+jon-lee1998@users.noreply.github.com>
2023-11-09 12:35:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,6 @@ my_cnx.close()
 if add_button:
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_cur = my_cnx.cursor()
-  streamlit.text(insert_row_snowflake(my_cur, fruit_add))
   display_fruit_data(my_cur)
   my_cnx.close()
 
