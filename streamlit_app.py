321434741fdf2060761c0e188703b1c54203abe6
jon-lee1998 <149186400+jon-lee1998@users.noreply.github.com>
2023-11-16 16:21:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,62 +1,20 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+import snowflake.connector as cn
 from urllib.error import URLError
 
 
-streamlit.header('test')
-myfruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-myfruitlist = myfruitlist.set_index('Fruit')
+connection = cn.(
+  user="1026303",
+  password="ElderBerry42!",
+  account="JPQYJYD-KEMPER",
+  warehouse="corp_wh",
+  role="useradmin"
+)
 
-fruits_selected = streamlit.multiselect("pick some fruits:", list(myfruitlist.index))
-fruits_to_show = myfruitlist.loc[fruits_selected]
-
-streamlit.dataframe(fruits_to_show)
-
-
-
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to display information about")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
-
-
-def get_fruit_load_list(cursor):
-  cursor.execute("select * from fruit_load_list")
-  return cursor.fetchall()
-
-def insert_row_snowflake(cursor, fruit_name):
-  cursor.execute("insert into fruit_load_list(fruit_name) values ('" + fruit_name + "')")
-  return "Thanks for adding " + fruit_name + " to the fruit list in Snowflake!"
-
-
-
-
-def display_fruit_data(cursor):
-  my_data_rows = get_fruit_load_list(my_cur)
-  return streamlit.dataframe(my_data_rows)
-    
-
-
-fruit_add = streamlit.text_input("What fruit would you like to add?")
-add_button = streamlit.button("Add fruit to list")
-display_button = streamlit.button("Show fruit list")
-if display_button:
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_cur = my_cnx.cursor()
-  display_fruit_data(my_cur)
-  my_cnx.close()
-if add_button:
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_cur = my_cnx.cursor()
-  insert_row_snowflake(my_cur, fruit_add)
-  my_cnx.close()
+cur = cn.cursor()
+cur.execute("select current_account()")
+streamlit.write(cur.fetchall())
 
 
